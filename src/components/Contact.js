faae7cd334919c22b624fe786f0d511003135b25
william <william.mallak1@gmail.com>
2022-08-03 17:59:40 +0200

    started on contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,38 +2,25 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div id='contact' >
-      Contact
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
-      <div>contact</div>
+    <div className='container-contact' id='contact' >
+      <form action="https://formspree.io/f/xnqleqrk" method='post'>
+        <label for='fullNames' >Name</label>
+          <input className='field' type='text' placeholder='Name' name='fullName' />
+        <label for='Email' >Email</label>
+          <input className='field' type='email' placeholder='Email' name='Email' />
+        <label for="message">how can I help you?</label>
+              <textarea required
+                name="message"
+                id="message"
+                cols="40"
+                rows="3"
+                placeholder="Please enter your message"
+              ></textarea>
+              <div class="submit">
+              <input type="submit" value="send" />
+              <input type="reset" value="reset" />
+            </div>
+      </form>
     </div>
   )
 }
