9644cb55ee144ba3360f831572a33765a6a760ea
william mallak <william.mallak1@gmail.com>
2023-07-20 15:29:51 +0200

    added the new angular project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import project1 from '../images/MovieApp.jpg';
 import project2 from '../images/finalProject.png';
 import project3 from '../images/graphql-project.png';
 import project4 from '../images/typescript-react-shopppingApp.png';
+import project5 from '../images/project5.png'
 
 export default function Projects() {
   return (
@@ -10,67 +11,83 @@ export default function Projects() {
       <h1>Personal Projects</h1>
       <div className="projects-container">
         <div className='project'>
-        <a
-          href="https://w-mallak-movieapp.netlify.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={project1} alt="project" />
-          <div className="project-name">MOVIE APP</div>
-          <p className="project-description">
-            A fully functional web app providing information about movies from
-            OMDB/TMDB APIs.{' '}
-          </p>
-        </a>
-        <a className='github-link' target='_blank' rel='noreferrer' href='https://github.com/William8421/movieapp'>Github</a>
+          <a
+            href="https://w-mallak-movieapp.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={project1} alt="project" />
+            <div className="project-name">MOVIE APP</div>
+            <p className="project-description">
+              A fully functional web app providing information about movies from
+              OMDB/TMDB APIs.{' '}
+            </p>
+          </a>
+          <a className='github-link' target='_blank' rel='noreferrer' href='https://github.com/William8421/movieapp'>Github</a>
         </div>
         <div className='project'>
-        <a
-          href="https://evconnection.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={project2} alt="project" />
-          <div className="project-name">
-            EV-<span>C</span>ONNECTION
-          </div>
-          <p className="project-description">
-            An App, where EV drivers can charge their electrical vehicles just
-            by connecting to wall-box owners in their surroundings.
-          </p>
-        </a>
-        <a className='github-link' target='_blank' rel='noreferrer' href='https://github.com/William8421/EV-Connection'>Github</a>
+          <a
+            href="https://evconnection.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={project2} alt="project" />
+            <div className="project-name">
+              EV-<span>C</span>ONNECTION
+            </div>
+            <p className="project-description">
+              An App, where EV drivers can charge their electrical vehicles just
+              by connecting to wall-box owners in their surroundings.
+            </p>
+          </a>
+          <a className='github-link' target='_blank' rel='noreferrer' href='https://github.com/William8421/EV-Connection'>Github</a>
         </div>
         <div className='project'>
-        <a
-          href="https://shopping-app-typescript.netlify.app/store"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={project4} alt="project" />
-          <div className="project-name">
-           SHOPPING-CART APP
-          </div>
-          <p className="project-description">
-            A fullstack e-commerce app created with typescript-react 
-          </p>
-        </a>
-        <a className='github-link' target='_blank' rel='noreferrer' href='https://github.com/William8421/typescript-shopping-app'>Github</a>
+          <a
+            href="https://shopping-app-typescript.netlify.app/store"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={project4} alt="project" />
+            <div className="project-name">
+              SHOPPING-CART APP
+            </div>
+            <p className="project-description">
+              A fullstack e-commerce app created with typescript-react
+            </p>
+          </a>
+          <a className='github-link' target='_blank' rel='noreferrer' href='https://github.com/William8421/typescript-shopping-app'>Github</a>
         </div>
         <div className='project'>
-        <a
-          href="https://github.com/William8421/MusicBandsGraphQl"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={project3} alt="project" />
-          <div className="project-name"> PLAYLIST APP </div>
-          <p className="project-description">
-            A fullstack app created with GraphQL-React to add songs and singers/bands to the
-            music playlist.
-          </p>
-        </a>
-        <a className='github-link' target='_blank' rel='noreferrer' href='https://github.com/William8421/MusicBandsGraphQl'>Github</a>
+          <a
+            href="https://angular-shopping-cart-app.netlify.app/store"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={project5} alt="project" />
+            <div className="project-name">
+              Angular SHOPPING-CART APP
+            </div>
+            <p className="project-description">
+              A Frontend e-commerce app created with Angular 14
+            </p>
+          </a>
+          <a className='github-link' target='_blank' rel='noreferrer' href='https://github.com/William8421/angular-shoppingCart'>Github</a>
+        </div>
+        <div className='project'>
+          <a
+            href="https://github.com/William8421/MusicBandsGraphQl"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={project3} alt="project" />
+            <div className="project-name"> PLAYLIST APP </div>
+            <p className="project-description">
+              A fullstack app created with GraphQL-React to add songs and singers/bands to the
+              music playlist.
+            </p>
+          </a>
+          <a className='github-link' target='_blank' rel='noreferrer' href='https://github.com/William8421/MusicBandsGraphQl'>Github</a>
         </div>
       </div>
     </div>
