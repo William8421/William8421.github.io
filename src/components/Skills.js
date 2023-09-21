653520517980c310fa2c90505784cb142e2af756
william mallak <william.mallak1@gmail.com>
2023-09-21 10:20:44 +0200

    added java and spring boot skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,27 +1,31 @@
-import htmlLogo from '../images/skills-logos/html.png';
-import cssLogo from '../images/skills-logos/css.png';
-import javascriptLogo from '../images/skills-logos/javascript.png';
-import reactLogo from '../images/skills-logos/react.png';
-import nodeJsLogo from '../images/skills-logos/nodejs.png';
-import expressLogo from '../images/skills-logos/express.png';
-import mongoDBLogo from '../images/skills-logos/mongodb.png';
-import ES6Logo from '../images/skills-logos/es6.png';
-import TypescriptLogo from '../images/skills-logos/ts-logo-512.png';
-import GraphQlLogo from '../images/skills-logos/GraphQLLogo.png';
-import angularLogo from '../images/skills-logos/angularlogo.png'
-import jestLogo from '../images/skills-logos/jest.png'
+import htmlLogo from "../images/skills-logos/html.png";
+import cssLogo from "../images/skills-logos/css.png";
+import javascriptLogo from "../images/skills-logos/javascript.png";
+import reactLogo from "../images/skills-logos/react.png";
+import nodeJsLogo from "../images/skills-logos/nodejs.png";
+import expressLogo from "../images/skills-logos/express.png";
+import mongoDBLogo from "../images/skills-logos/mongodb.png";
+import ES6Logo from "../images/skills-logos/es6.png";
+import TypescriptLogo from "../images/skills-logos/ts-logo-512.png";
+import GraphQlLogo from "../images/skills-logos/GraphQLLogo.png";
+import angularLogo from "../images/skills-logos/angularlogo.png";
+import jestLogo from "../images/skills-logos/jest.png";
+import javaLogo from "../images/skills-logos/java.png";
+import springBootLogo from "../images/skills-logos/spring-boot.png";
 
 export const logos = [
-    { img: htmlLogo, name: 'HTML5' },
-    { img: cssLogo, name: 'CSS3' },
-    { img: javascriptLogo, name: 'JavaScript' },
-    { img: TypescriptLogo, name: 'TypeScript' },
-    { img: reactLogo, name: 'React' },
-    { img: angularLogo, name: 'Angular' },
-    { img: nodeJsLogo, name: 'Node.js' },
-    { img: expressLogo, name: 'Express.js' },
-    { img: mongoDBLogo, name: 'MongoDB' },
-    { img: ES6Logo, name: 'ES6' },
-    { img: GraphQlLogo, name: 'GraphQl' },
-    { img: jestLogo, name: 'Jest' },
+  { img: htmlLogo, name: "HTML5" },
+  { img: cssLogo, name: "CSS3" },
+  { img: javascriptLogo, name: "JavaScript" },
+  { img: TypescriptLogo, name: "TypeScript" },
+  { img: reactLogo, name: "React" },
+  { img: angularLogo, name: "Angular" },
+  { img: nodeJsLogo, name: "Node.js" },
+  { img: expressLogo, name: "Express.js" },
+  { img: mongoDBLogo, name: "MongoDB" },
+  { img: ES6Logo, name: "ES6" },
+  { img: javaLogo, name: "Java" },
+  { img: springBootLogo, name: "Spring Boot" },
+  { img: GraphQlLogo, name: "GraphQl" },
+  { img: jestLogo, name: "Jest" },
 ];
