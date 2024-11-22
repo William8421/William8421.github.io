9cd5040c42a381b18ed5a7f95ec7fe4f10e3a86c
william mallak <william.mallak1@gmail.com>
2024-11-22 11:52:40 +0100

    added docker and aws skills, rearanged skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,33 +3,37 @@ import cssLogo from "../images/skills-logos/css.png";
 import javascriptLogo from "../images/skills-logos/javascript.png";
 import reactLogo from "../images/skills-logos/react.png";
 import nodeJsLogo from "../images/skills-logos/nodejs.png";
-import expressLogo from "../images/skills-logos/express.png";
+// import expressLogo from "../images/skills-logos/express.png";
+// import ES6Logo from "../images/skills-logos/es6.png";
 import postgreSQLLogo from "../images/skills-logos/postgreSQL.png";
 import mongoDBLogo from "../images/skills-logos/mongodb.png";
-import ES6Logo from "../images/skills-logos/es6.png";
 import TypescriptLogo from "../images/skills-logos/ts-logo-512.png";
 import GraphQlLogo from "../images/skills-logos/GraphQLLogo.png";
-import angularLogo from "../images/skills-logos/angularlogo.png";
+import angularLogo from "../images/skills-logos/angular.png";
 import VueLogo from "../images/skills-logos/vue-logo.png";
 import jestLogo from "../images/skills-logos/jest.png";
 import javaLogo from "../images/skills-logos/java.png";
 import springBootLogo from "../images/skills-logos/spring-boot.png";
+import dockerLogo from "../images/skills-logos/docker.png";
+import AWSLogo from "../images/skills-logos/AWS.png";
 
 export const logos = [
-  { img: htmlLogo, name: "HTML5" },
-  { img: cssLogo, name: "CSS3" },
+  { img: htmlLogo, name: "HTML" },
+  { img: cssLogo, name: "CSS" },
   { img: javascriptLogo, name: "JavaScript" },
   { img: TypescriptLogo, name: "TypeScript" },
   { img: reactLogo, name: "React" },
   { img: angularLogo, name: "Angular" },
   { img: VueLogo, name: "Vue3" },
   { img: nodeJsLogo, name: "Node.js" },
-  { img: expressLogo, name: "Express.js" },
-  { img: mongoDBLogo, name: "MongoDB" },
-  { img: ES6Logo, name: "ES6" },
-  { img: postgreSQLLogo, name: "PostgreSQL" },
+  // { img: expressLogo, name: "Express.js" },
+  // { img: ES6Logo, name: "ES6" },
   { img: javaLogo, name: "Java" },
   { img: springBootLogo, name: "Spring Boot" },
   { img: GraphQlLogo, name: "GraphQl" },
+  { img: mongoDBLogo, name: "MongoDB" },
+  { img: postgreSQLLogo, name: "PostgreSQL" },
   { img: jestLogo, name: "Jest" },
+  { img: dockerLogo, name: "Docker" },
+  { img: AWSLogo, name: "AWS" },
 ];
